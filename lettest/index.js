5e8aed668b78a750c8ee891c30b82e4c90a9421c
SvenLa <sven.lammertink@hotmail.com>
2022-02-11 19:00:11 +0100

    Lettest game added

---
+++ lettest/index.js
@@ -0,0 +1,31 @@
+let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letters = document.getElementsByClassName('l');
+let l0 = "A", l1 = "B";
+
+function genLetter(non=30) {
+    let l = Math.floor(Math.random()*26)
+    if (l===non) {return genLetter(non)}
+    return l
+}
+
+function newTurn() {
+    l0 = genLetter();
+    l1 = genLetter(l0);
+    letters[0].innerHTML = alphabet[l0]
+    letters[1].innerHTML = alphabet[l1]
+}
+
+let WINS = parseInt(getCookie("Lettest_WINS")) || 0;
+let LOSS = parseInt(getCookie("Lettest_LOSS")) || 0;
+
+function sla_button(input) {
+    let isCorrect = l0>l1 ? "AFTER" === input : "BEFORE"===input;
+    let resultBanners = document.getElementsByClassName('result');
+    for (let resultBanner of resultBanners) {resultBanner.style.display = 'none';}
+    document.getElementsByClassName("result")[isCorrect+0].style.display='inherit';
+    let stats;
+    stats = isCorrect ? WINS+=1: LOSS+=1;
+    setCookie("FUN", isCorrect ? "Lettest_WINS": "Lettest_LOSS", stats, 365);
+    document.getElementById('winPercent').innerHTML = "&nbsp;."+(WINS/(WINS+LOSS)*100).toFixed(0)+"&#127942;"
+    newTurn()
+}
---
+++ script/cookie.js
@@ -0,0 +1,33 @@
+function acceptCookies(levels){
+    if(levels.includes('FUN')) {setCookie('FUN', 'FUN', 'Y', 365)}
+    document.getElementById('cookieBannerContainer').style.display = 'none';
+}
+
+if (getCookie('FUN')==='Y') {
+    document.getElementById('cookieBannerContainer').style.display = 'none';
+}
+
+function setCookie(level, cookie_name, cookie_value, expiration) {
+    //FUN, ANA, ADS
+    if(level.toUpperCase()!=="FUN") {return}
+    const d = new Date();
+    d.setTime(d.getTime() + (expiration*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cookie_name + "=" + cookie_value + ";" + expires + ";path=/";
+}
+
+function getCookie(cookie_name) {
+    let name = cookie_name + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
