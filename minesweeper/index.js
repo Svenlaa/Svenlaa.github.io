0ee32f2e7ec4022bf3328d21960cad6374c53914
SvenLa <sven.lammertink@hotmail.com>
2022-02-21 17:13:59 +0100

    Make the code more readable
    This change will undo most of the minifying that has been done, thus
    making the code more readable, with better named variables

--- minesweeper/index.js
+++ minesweeper/index.js
@@ -1,114 +1,128 @@
-document.oncontextmenu = (() => !1);
+document.oncontextmenu = (() => false);
 
-function pad(n) {
-    return n.toString().padStart(2, "0")
+function pad(number) {
+    return number.toString().padStart(2, "0")
 }
 
-const selectID = document.getElementById("selectedItem")
-    , headerID = document.getElementById("header")
-    , flagsID = document.getElementById("flagLeft")
-    , mainEle = document.getElementsByTagName("main")[0]
-    , gameStateID = document.getElementById("gameState")
-    , minGridSize = 50
-    , timer = setInterval(updateTime, 1e3);
-let xCells = 0
-    , cellSize = 0
-    , yCells = 0
-    , totalBombs = 0
-    , cellPlaces = []
-    , timePlayed = 0
-    , noMove = !1
-    , shownFields = 0;
+const selectID = document.getElementById("selectedItem");
+const headerID = document.getElementById("header");
+const flagsID = document.getElementById("flagLeft");
+const mainEle = document.getElementsByTagName("main")[0];
+const gameStateID = document.getElementById("gameState");
+const minGridSize = 50;
+const timer = setInterval(updateTime, 1e3);
+let xCells = 0;
+let cellSize = 0;
+let yCells = 0;
+let totalBombs = 0;
+let cellNumbers = []; // Holds the number of each cell, bomb = 9
+let timePlayed = 0;
+let noMove = false;
+let shownFields = 0;
 
 function updateTime() {
-    let e = ++timePlayed % 60
-        , l = Math.floor(timePlayed / 60);
-    document.getElementById("timePlayed").innerHTML = `${l}:${pad(e)}`
+    let m = timePlayed++ % 60;
+    let h = Math.floor(timePlayed / 60);
+    document.getElementById("timePlayed").innerHTML = `${h}:${pad(m)}`
 }
 
-function cellClick(e, l) {
-    let t = e.classList
-        , s = [...e.parentNode.children].indexOf(e);
-    if (!t.contains("show") && !noMove)
-        if (flagSelected || l)
-            t.contains("flag") ? i(t, !1) : i(t, !0);
-        else if (t.contains("flag"))
-            i(t, 0);
-        else {
-            if (t.add("show"),
-                t.add("o" + cellPlaces[s]),
+function cellClick(cellElement, isContextClick) {
+    let classList = cellElement.classList;
+    let cellIndex = [...cellElement.parentNode.children].indexOf(cellElement);
+    if (!(classList.contains("show") || noMove)) {
+        if (flagSelected || isContextClick) {
+            classList.contains("flag") ? swapFlag(classList, false) : swapFlag(classList, true);
+        } else if (classList.contains("flag")) {
+            swapFlag(classList, 0);
+        } else {
+            if (classList.add("show"),
+                classList.add("o" + cellNumbers[cellIndex]),
                 shownFields++,
-            0 == cellPlaces[s]) {
-                let l = [s + xCells, s - xCells];
-                s % xCells != 0 && l.push(s - 1, s - 1 - xCells, s - 1 + xCells),
-                (s + 1) % xCells != 0 && l.push(s + 1, s + 1 - xCells, s + 1 + xCells),
-                    l.forEach(l => {
-                            0 <= l && l < xCells * yCells && cellClick([...e.parentNode.children][l], !1)
-                        }
-                    )
+            0 == cellNumbers[cellIndex]
+            ) {
+                let surroundingCells = [cellIndex + xCells, cellIndex - xCells];
+                cellIndex % xCells != 0 && surroundingCells.push(cellIndex - 1, cellIndex - 1 - xCells, cellIndex - 1 + xCells);
+                (cellIndex + 1) % xCells != 0 && surroundingCells.push(cellIndex + 1, cellIndex + 1 - xCells, cellIndex + 1 + xCells);
+                surroundingCells.forEach(l => {
+                    l >= 0 && xCells * yCells > l && cellClick([...cellElement.parentNode.children][l], !1)
+                })
             }
-            9 == cellPlaces[s] ? a("red", ":-(") : xCells * yCells - totalBombs == shownFields && a("gold", ":-)")
+            cellNumbers[cellIndex] != 9 ?
+                xCells * yCells - totalBombs == shownFields && finishGame("gold", "&#x1F601") :
+                finishGame("red", "&#x1F615");
         }
-
-    function i(e, l) {
-        l ? (flagsID.innerHTML--,
-            e.add("flag")) : l || (e.remove("flag"),
-            flagsID.innerHTML++)
     }
 
-    function a(e, l) {
-        gameStateID.innerHTML = l,
-            headerID.style.backgroundColor = e,
-            selectID.style.backgroundColor = "#0000",
-            clearInterval(timer),
-            noMove = !0
+    function swapFlag(classList, hasFlag) {
+        hasFlag ?
+            flagsID.innerHTML-- && classList.add("flag") :
+            flagsID.innerHTML++ && classList.remove("flag");
+    }
+
+    function finishGame(color, emoji) {
+        gameStateID.innerHTML = emoji
+        headerID.style.backgroundColor = color
+        selectID.style.backgroundColor = "#0000"
+        clearInterval(timer)
+        noMove = true
     }
 }
 
-function drawCells(e) {
-    headerID.style.height = "auto",
-        mainEle.style.height = "100%",
-        xCells = Math.floor(mainEle.offsetWidth / minGridSize),
-        cellSize = mainEle.offsetWidth / xCells,
-        yCells = Math.floor(mainEle.offsetHeight / cellSize);
-    const l = xCells * yCells;
-    mainEle.style.gridTemplateColumns = `repeat(${xCells}, ${cellSize}px)`,
-        mainEle.style.gridTemplateRows = `repeat(${yCells}, ${cellSize}px)`,
-        headerID.style.height = "100%",
-        mainEle.style.height = "auto",
-        totalBombs = Math.floor(xCells * yCells * e);
-    let t = [];
-    for (i = 0; i < totalBombs; i++) {
-        function s() {
-            let e = Math.floor(Math.random() * xCells * yCells);
-            t.includes(e) ? s() : t.push(e)
-        }
+function drawCells(bombChance) {
+    function spawnBomb() {
+        let randomCell = Math.floor(Math.random() * xCells * yCells);
+        allBombs.includes(randomCell) ? spawnBomb() : allBombs.push(randomCell)
+    }
 
-        s()
+    headerID.style.height = "auto"
+    mainEle.style.height = "100%"
+    xCells = Math.floor(mainEle.offsetWidth / minGridSize)
+    cellSize = mainEle.offsetWidth / xCells
+    yCells = Math.floor(mainEle.offsetHeight / cellSize)
+    const totalCells = xCells * yCells;
+    mainEle.style.gridTemplateColumns = `repeat(${xCells}, ${cellSize}px)`
+    mainEle.style.gridTemplateRows = `repeat(${yCells}, ${cellSize}px)`
+    headerID.style.height = "100%"
+    mainEle.style.height = "auto"
+    totalBombs = Math.floor(totalCells * bombChance)
+    let allBombs = [];
+    for (bombsPlaced = 0; bombsPlaced < totalBombs; bombsPlaced++) {
+        spawnBomb()
     }
-    for (i = 0; i < l; i++) {
-        let e = document.createElement("div");
-        e.classList.add("gridCell");
-        let s = [i - xCells];
-        i % xCells != 0 && s.push(i - 1, i - 1 - xCells),
-        (i + 1) % xCells != 0 && s.push(i + 1 - xCells),
-            t.includes(i) ? (cellPlaces[i] = 9,
-                s.forEach(e => {
-                        0 <= e && e < l && 9 != cellPlaces[e] && cellPlaces[e]++
-                    }
-                )) : (cellPlaces[i] = 0,
-                s.forEach(e => {
-                        0 <= e && e < l && 9 == cellPlaces[e] && cellPlaces[i]++
-                    }
-                )),
-            e.addEventListener("click", function () {
-                cellClick(this, !1)
-            }),
-            e.addEventListener("contextmenu", function () {
-                cellClick(this, !0)
-            }),
-            mainEle.appendChild(e)
+    for (i = 0; i < totalCells; i++) {
+        let cellEle = document.createElement("div");
+        cellEle.classList.add("gridCell");
+        let surroundingCells = [i - xCells];
+        if (i % xCells != 0) {
+            surroundingCells.push(i - 1, i - 1 - xCells)
+        }
+        if ((i + 1) % xCells != 0) {
+            surroundingCells.push(i + 1 - xCells)
+        }
+        if (allBombs.includes(i)) {
+            cellNumbers[i] = 9;
+            surroundingCells.forEach(cell => {
+                if (cell >= 0 && totalCells > cell && cellNumbers[cell] != 9) {
+                    cellNumbers[cell]++
+                }
+            })
+        } else {
+            cellNumbers[i] = 0;
+            surroundingCells.forEach(cell => {
+                if (cell >= 0 && totalCells > cell && cellNumbers[cell] == 9) {
+                    cellNumbers[i]++
+                }
+            })
+        }
+        cellEle.addEventListener("click", function () {
+            cellClick(this, false)
+        });
+        cellEle.addEventListener("contextmenu", function () {
+            cellClick(this, true)
+        });
+        mainEle.appendChild(cellEle);
     }
+    console.log(cellNumbers)
     flagsID.innerHTML = totalBombs
 }
 
@@ -116,7 +130,13 @@ drawCells(.15);
 let flagSelected = !1;
 
 function _selectFlag() {
-    noMove || (flagSelected ? (flagSelected = !1,
-        selectID.style.backgroundColor = "red") : flagSelected || (flagSelected = !0,
-        selectID.style.backgroundColor = "forestGreen"))
+    if (!noMove) {
+        if (flagSelected) {
+            selectID.style.backgroundColor = "red";
+            flagSelected = false
+        } else {
+            selectID.style.backgroundColor = "forestGreen";
+            flagSelected = true
+        }
+    }
 }
